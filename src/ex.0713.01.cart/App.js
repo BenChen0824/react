9e2080295558eab6dca7b709bb6ebac3b6abbe1f
BenChen0824 <104603512+BenChen0824@users.noreply.github.com>
2022-07-13 16:16:40 +0800

    cart

---
+++ src/ex.0713.01.cart/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cart from './ex.0713.01.cart/Cart';
+
+function App() {
+    return (
+        <>
+            <Cart />
+        </>
+    );
+}
+
+export default App;
