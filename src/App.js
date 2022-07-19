6995312c937f706e8d3d3123d219f957f555b469
BenChen0824 <104603512+BenChen0824@users.noreply.github.com>
2022-07-19 14:55:27 +0800

    BS React-BS SASS Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
-import UserData from './ex.0718.02.userData/userData';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
     return (
         <>
-            <UserData />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="cart" element={<Cart />} />
+                    <Route path="product" element={<Product />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
--- src/ex.0718.02.userData/userData.js
+++ src/ex.0718.02.userData/userData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './userData.css';
+//import _ from 'lodash'
 
 // 要裝axios套件: `yarn add axios`
 import axios from 'axios';
@@ -10,6 +11,13 @@ import axios from 'axios';
 //     "name": "張佳蓉",
 //     "birth": "990101"
 //  },
+
+// chunk - 依size分成子陣列，ex. chunk([1, 2, 3, 4, 5], 2) -> [[1,2],[3,4],[5]]
+// https://stackoverflow.com/questions/8495687/split-array-into-chunks
+const chunk = (arr, size) =>
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+        arr.slice(i * size, i * size + size)
+    );
 
 function UserData() {
     // 第一次記錄伺服器的原始資料用
@@ -22,14 +30,29 @@ function UserData() {
     // 搜尋用
     const [searchWord, setSearchWord] = useState('');
 
+    // 分頁用
+    // pageNow 目前頁號
+    // perPage 每頁多少數量
+    // pageTotal 目前有多少頁
+    const [pageNow, setPageNow] = useState(1); // 預設第一頁
+    const [perPage, setPerPage] = useState(5); // 預設一頁5筆資料
+    const [pageTotal, setPageTotal] = useState(0); // 等伺服器抓完資料才知道多少(didMount時決定)
+
     const getUserData = async () => {
         const response = await axios.get(
             'https://my-json-server.typicode.com/eyesofkids/json-fake-data/users'
         );
         //console.log(response)
         // 設定到state
-        setUsersDisplay(response.data);
+
         setUsersRaw(response.data);
+
+        const pageArray = chunk(response.data, perPage);
+
+        if (pageArray.length > 0) {
+            setPageTotal(pageArray.length);
+            setUsersDisplay(pageArray);
+        }
     };
 
     // didMount
@@ -73,17 +96,57 @@ function UserData() {
                 </tr>
             </thead>
             <tbody>
-                {usersDisplay.map((v, i) => {
-                    return (
-                        <tr key={v.id}>
-                            <td>{v.id}</td>
-                            <td>{v.name}</td>
-                            <td>{v.birth}</td>
-                        </tr>
-                    );
-                })}
+                {usersDisplay.length > 0 &&
+                    usersDisplay[pageNow - 1].map((v, i) => {
+                        return (
+                            <tr key={v.id}>
+                                <td>{v.id}</td>
+                                <td>{v.name}</td>
+                                <td>{v.birth}</td>
+                            </tr>
+                        );
+                    })}
             </tbody>
         </table>
+    );
+
+    const paginationBar = (
+        <>
+            <div className="pagination">
+                <a
+                    href="#/"
+                    onClick={() => {
+                        setPageNow(1);
+                    }}
+                >
+                    &laquo;
+                </a>
+                {Array(pageTotal)
+                    .fill(1)
+                    .map((v, i) => {
+                        return (
+                            <a
+                                key={i}
+                                href="#/"
+                                className={i + 1 === pageNow ? 'active' : ''}
+                                onClick={() => {
+                                    setPageNow(i + 1);
+                                }}
+                            >
+                                {i + 1}
+                            </a>
+                        );
+                    })}
+                <a
+                    href="#/"
+                    onClick={() => {
+                        setPageNow(pageTotal);
+                    }}
+                >
+                    &raquo;
+                </a>
+            </div>
+        </>
     );
 
     return (
@@ -94,26 +157,60 @@ function UserData() {
                 placeholder="輸入姓名"
                 value={searchWord}
                 onChange={(e) => {
-                    setSearchWord(e.target.value);
-                }}
-            />
-            <button
-                onClick={() => {
-                    if (searchWord) {
+                    const newSearchWord = e.target.value;
+
+                    setSearchWord(newSearchWord);
+
+                    // 開啟載入指示動態
+                    setIsLoading(true);
+
+                    if (newSearchWord) {
                         const newUsersDisplay = usersRaw.filter((v, i) =>
-                            v.name.includes(searchWord)
+                            v.name.includes(newSearchWord)
                         );
 
-                        setUsersDisplay(newUsersDisplay);
+                        const pageArray = chunk(newUsersDisplay, perPage);
+
+                        if (pageArray.length > 0) {
+                            setPageTotal(pageArray.length);
+                            setUsersDisplay(pageArray);
+                        }
+
+                        // setUsersDisplay(newUsersDisplay)
                     } else {
                         // reset
-                        setUsersDisplay(usersRaw);
+                        const pageArray = chunk(usersRaw, perPage);
+
+                        if (pageArray.length > 0) {
+                            setPageTotal(pageArray.length);
+                            setUsersDisplay(pageArray);
+                        }
                     }
                 }}
-            >
-                搜尋
-            </button>
+            />
+            {/* <button
+        onClick={() => {
+          // 開啟載入指示動態
+          setIsLoading(true)
+
+          if (searchWord) {
+            const newUsersDisplay = usersRaw.filter((v, i) =>
+              v.name.includes(searchWord)
+            )
+
+            setUsersDisplay(newUsersDisplay)
+          } else {
+            // reset
+            setUsersDisplay(usersRaw)
+          }
+        }}
+      >
+        搜尋
+      </button> */}
+            <hr />
             {isLoading ? spinner : displayTable}
+            <br />
+            {paginationBar}
         </>
     );
 }
